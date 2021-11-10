e7d983b7abef39e52a18252d52703d3537de88cc
Jonatas Prates <jonatasluisprates@gmail.com>
2021-11-09 21:53:32 -0300

    criando home e navbar e footer

--- costs/src/App.js
+++ costs/src/App.js
@@ -1,30 +1,27 @@
-import {BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom'
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 import Company from './components/pages/Company';
 import Contact from './components/pages/Contact';
 import Home from './components/pages/Home';
 import NewProjetct from './components/pages/NewProjetct';
 
 import Container from './components/layout/Container';
+import NavBar from './components/layout/Navbar';
+import Footer from './components/layout/Footer';
 
 
 function App() {
   return (
     <Router>
-      <div>
-        <Link to="/">Home</Link>
-        <Link to="/contact">Contato</Link>
-        <Link to="/company">Empresa</Link>
-        <Link to="/newproject">Novo Projeto</Link>
-      </div>
+      <NavBar/>
       <Container customClass="min-height">
         <Routes>
               <Route exact path='/' element={<Home/>}/>
-              <Route path="/company" element={<Company/>}> </Route>
-              <Route path="/contact" element={<Contact/>}> </Route>
-              <Route path="/newproject" element={<NewProjetct/>}></Route>
+              <Route path="/company" element={<Company/>}/>
+              <Route path="/contact" element={<Contact/>}/>
+              <Route path="/newproject" element={<NewProjetct/>}/>
         </Routes>
       </Container>
-      <p>Footer</p>
+      <Footer/>
     </Router>
   );
 }
